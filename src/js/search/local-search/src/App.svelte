38fa97039163446baa437abc49cb3a38d2b4819b
fengkx <liangkx8237@gmail.com>
2021-06-23 17:51:11 +0800

    format code

--- src/js/search/local-search/src/App.svelte
+++ src/js/search/local-search/src/App.svelte
@@ -1,39 +1,59 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	const el = document.getElementById('content-json');
-	const placeholder = el.getAttribute('data-placeholder')
-	const jsonPath = el.textContent;
-	let posts = [];
-	let keyword = '';
-	const fetchData = async (jsonPath) => {
-		const resp = await fetch(jsonPath);
-		const data = await resp.json();
-		return data.posts;
+  import { onMount } from "svelte";
+  const el = document.getElementById("content-json");
+  const placeholder = el.getAttribute("data-placeholder");
+  const jsonPath = el.textContent;
+  let posts = [];
+  let input = "";
+  $: keywords = input.trim().toLocaleLowerCase().split(/[\s\-]+/)
+  const fetchData = async (jsonPath) => {
+    const resp = await fetch(jsonPath);
+    const data = await resp.json();
+    return data.posts;
+  };
+
+  onMount(async () => {
+    posts = await fetchData(jsonPath);
+  });
+
+  let searchPosts;
+  $: {
+  	const results = [];
+  	for(let i=0, len=posts.length;i< len;i++) {
+  		const post = posts[i];
+  		const text = post.text.trim().replace(/<[^>]+>/g, '').toLowerCase();
+  		const title = post.title.trim().localeCompare()
+  		if(title.length > 0 && text.length > 0) {
+
+		}
 	}
-
-	onMount(async () => {
-		posts = await fetchData(jsonPath);
-	})
-
-	$: searchPosts = posts
-			.filter((p) => p.title.includes(keyword) || p.text.includes(keyword))
-			.map((p) => {
-				const r = { ...p };
-				// r.text = `${p.text.substring(0, 100)}...`;
-				return r;
-			});
+  }
 </script>
+
 <div>
-	<div class="search-header bg-gray-400">
-		<input id="actual-search-input" model="keyword" ref="input" class="inline-block w-full h-10 px-2 py-1" placeholder="{placeholder}" type="text" bind:value={keyword} >
-	</div>
-	<div class="search-result bg-gray-200">
-		{#each searchPosts as post}
-		<a href="/{ post.path }" class="result-item block px-2 pb-3 mb-1 pt-1 hover:bg-indigo-100">
-			<i class="iconfont icon-file"></i>
-			<h1 class="result-title inline font-medium text-lg">{ post.title }</h1>
-			<p class="result-content line-clamp-4 text-gray-600 text-sm">{ post.text }</p>
-		</a>
-		{/each}
-	</div>
+  <div class="search-header bg-gray-400">
+    <input
+      id="actual-search-input"
+      model="keyword"
+      ref="input"
+      class="inline-block w-full h-10 px-2 py-1"
+      {placeholder}
+      type="text"
+      bind:value={input}
+    />
+  </div>
+  <div class="search-result bg-gray-200">
+    {#each searchPosts as post}
+      <a
+        href="/{post.path}"
+        class="result-item block px-2 pb-3 mb-1 pt-1 hover:bg-indigo-100"
+      >
+        <i class="iconfont icon-file" />
+        <h1 class="result-title inline font-medium text-lg">{post.title}</h1>
+        <p class="result-content line-clamp-4 text-gray-600 text-sm">
+          {post.text}
+        </p>
+      </a>
+    {/each}
+  </div>
 </div>
